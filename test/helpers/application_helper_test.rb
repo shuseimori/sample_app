52767081c442be7786a3ca1da80733cdc8743ace
Shusei Mori <shusei.mori777@gmail.com>
2018-04-10 08:19:49 +0000

    Implement basic login

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -4,4 +4,5 @@ class ApplicationHelperTest < ActionView::TestCase
     test "full title helper" do
         assert_equal full_title, "Ruby on Rails Tutorial Sample App"
     end
+    
 end
---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+ 
+ def setup
+  @user = users(:Shusei)
+ end
+ 
+ test "login with valid information followed by logout" do
+ get login_path
+ post login_path, params: { session: {email:@user.email,
+                                      password:"moripon9"}}
+ assert_redirected_to @user
+ follow_redirect!
+ assert_template "users/show"
+ 
+ delete logout_path
+ assert_not is_logged_in?
+ assert_redirected_to root_url
+ follow_redirect!
+ 
+ end
+ 
+ test "flash should disappear soon" do
+   get login_path
+   assert_template "sessions/new"
+   post login_path, params:{session:{email:"", password:""}}
+   assert_template "sessions/new"
+   assert_not flash.empty?
+   get root_path
+   assert flash.empty?
+ end
+end
