1a73e99cc6cf489d31edbcaa628ad920ae32db37
Shusei Mori <shusei.mori777@gmail.com>
2018-04-11 11:18:35 +0000

    Implement advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -17,6 +17,9 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
  delete logout_path
  assert_not is_logged_in?
  assert_redirected_to root_url
+ 
+  # 2番目のウィンドウでログアウトをクリックするユーザーをシミュレートする
+    delete logout_path
  follow_redirect!
  
  end
